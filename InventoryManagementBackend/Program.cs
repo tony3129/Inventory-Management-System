a06feefdf324ea06ae0af026d54878d503fdc8c7
Tony Liu <tliu55@myseneca.ca>
2025-01-19 09:54:45 -0500

    included dotenv.net, InventoryManagementBackend, and EntityFrameworkCore packages

--- InventoryManagementBackend/Program.cs
+++ InventoryManagementBackend/Program.cs
@@ -1,8 +1,22 @@
+using InventoryManagementBackend.Data;
+using Microsoft.EntityFrameworkCore;
+using dotenv.net;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//load env variables from privateInfo
+DotEnv.Load();
+
+//configure database context
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options=>{
+    options.UseNpgsql(connectionString);
+})
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
